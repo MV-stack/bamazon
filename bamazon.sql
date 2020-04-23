e3005deadf4ffb47d64c72f20558c840345ae5b1
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-04-22 17:46:23 -0700

    debugged bamazonCustomer.js, updated readme, added .gitignore

--- bamazon.sql
+++ bamazon.sql
@@ -14,7 +14,16 @@ CREATE TABLE products (
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("Hand Sanitizer (67.6 fl.oz.)", "Cleaning Supplies", 25.00, 1000), 
+("Bleach (121 oz.)", "Cleaning Supplies", 15.00, 2500), 
+("Toilet Paper (30 ct.)", "Cleaning Supplies", 25.00, 3000), 
+("N95 mask (1 ct.)", "Safety", 10.00, 2500), 
+("Surgical Masks (50 ct.)", "Safety", 40.00, 2000),
+("Ramen Noodle Soup (1 ct.)", "Groceries", 1.25, 10000),
+("Avon M50 CBRN Gas Mask (1 ct)", "Safety", 230.00, 2500),
+("M50 Gas Mask Primary Filter Set (2 ct.)", "Safety", 45.00, 6500),
+("Hershey Chocolate Bar (1 ct.)", "Groceries", 2.00, 10000),
+("Clorox Wipes (75 ct.)", "Cleaning Supplies", 10.00, 2500);
 
 
 
---
+++ bamazonCustomer.js
@@ -0,0 +1,79 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazonDB"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId);
+    showProducts();
+});
+
+function showProducts() {
+    console.log("Items available for sale: \n");
+    connection.query("SELECT * FROM Products", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+       askCustomer();
+    });
+}
+function askCustomer() {
+    inquirer.prompt([
+        {type: "number",
+        message: "What items do you want to purchase?",
+        name: "item"
+        },
+        {type: "number",
+        message: "How many?",
+        name: "quantity"
+        }        
+    ]) .then(function(response){
+        console.log(response);
+        checkInventory(response);
+        // go to the db where the item_id === your item and verify the stock
+        //if stcok < the quatity then yo don't have enought 
+        // if enough then sell it and update the db 
+    })    
+}
+function checkInventory(response) {
+    connection.query("SELECT * FROM Products WHERE item_id = " + response.item, function(err, res) {
+        console.log(res);
+        if(res[0].stock_quantity < response.quantity) {
+            console.log("Insufficient quantity!");
+            showProducts()
+        }
+        else{
+            console.log("Order placed! Total price: ", res[0].price * response.quantity);
+            var newQ = res[0].stock_quantity - response.quantity
+            // update
+
+            connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                  {
+                    stock_quantity: newQ
+                  },
+                  {
+                    item_id: response.item
+                  }
+                ],
+                function(err, res) {
+                  if (err) throw err;
+                  console.log(res.affectedRows + " products updated!\n");
+                  // Call deleteProduct AFTER the UPDATE completes
+                  showProducts();
+                }
+              );
+
+
+        }
+    })
+
+}
